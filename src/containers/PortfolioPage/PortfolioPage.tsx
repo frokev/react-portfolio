9582808557e9e2fbafe21d3bd36020da645ef4fb
kevin <frotor15@westerdals.gmail.com>
2019-02-15 16:11:21 +0100

    portfolio page mostly finished, small responsiveness issues

--- src/containers/PortfolioPage/PortfolioPage.tsx
+++ src/containers/PortfolioPage/PortfolioPage.tsx
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
+import portfolioImg from "../../assets/portfolio-img.jpg";
 
 export default class PortfolioPage extends Component {
   render() {
-    return <div />;
+    return (
+      <div className="portfolio-page">
+        <div className="content-wrapper">
+          <h1 className="page-title">My awesome portfolio</h1>
+          <div className="cards-wrapper">
+            <div className="card">
+              <div className="image-wrapper">
+                <img src={portfolioImg} alt="Portfolio image" />
+                <h2 className="card-title">Best project ever</h2>
+              </div>
+              <div className="paragraph-wrapper">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at porttitor sem.  Aliquam erat volutpat.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="image-wrapper">
+                <img src={portfolioImg} alt="Portfolio image" />
+                <h2 className="card-title">Best project ever</h2>
+              </div>
+              <div className="paragraph-wrapper">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at porttitor sem.  Aliquam erat volutpat.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="image-wrapper">
+                <img src={portfolioImg} alt="Portfolio image" />
+                <h2 className="card-title">Best project ever</h2>
+              </div>
+              <div className="paragraph-wrapper">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at porttitor sem.  Aliquam erat volutpat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }

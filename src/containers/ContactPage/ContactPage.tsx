ba46bf21336a28cf80293f9786a682f3882031c4
kevin <frotor15@westerdals.gmail.com>
2019-02-18 16:39:34 +0100

    Finished ContactPage responsive design, and fixed some style bugs

--- src/containers/ContactPage/ContactPage.tsx
+++ src/containers/ContactPage/ContactPage.tsx
@@ -1,40 +1,74 @@
-import React, { Component } from "react";
+import React, {
+  Component,
+  InputHTMLAttributes,
+  ChangeEvent,
+  FormEvent
+} from "react";
 
 export default class ContactPage extends Component {
+  state = {
+    email: undefined,
+    name: undefined,
+    message: undefined
+  };
+
   render() {
     return (
       <div className="contact-page">
         <div className="content-wrapper">
           <h1 className="page-title">Contact</h1>
-          <div className="form-wrapper">
-            <div className="input-section">
-              <p className="input-title">Email</p>
+          <form onSubmit={this.handleSubmit} className="form-wrapper">
+            <label className="input-section">
+              <span className="input-title">Email</span>
               <input
                 type="email"
+                name="email"
                 placeholder="your@email.com"
+                value={this.state.email}
+                onChange={this.handleFormChange}
                 className="input-field"
               />
-            </div>
-            <div className="input-section">
-              <p className="input-title">Name</p>
+            </label>
+            <label className="input-section">
+              <span className="input-title">Name</span>
               <input
                 type="text"
+                name="name"
                 placeholder="Justin Bieber"
                 className="input-field"
               />
-            </div>
-            <div className="input-section">
-              <p className="input-title">Message</p>
-              <input
-                type="text"
+            </label>
+            <label className="input-section">
+              <span className="input-title">Message</span>
+              <textarea
+                name="message"
                 placeholder="How do you like my latest album?"
                 className="message-field"
               />
-            </div>
-            <button className="submit-buttion" />
-          </div>
+            </label>
+            <input type="submit" value="Send" className="submit-button" />
+          </form>
         </div>
       </div>
     );
   }
+
+  handleFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name == "email")
+      this.setState({
+        email: event.target.value
+      });
+    else if (event.target.name == "name")
+      this.setState({
+        name: event.target.value
+      });
+    else if (event.target.name == "message")
+      this.setState({
+        message: event.target.value
+      });
+  };
+
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 }
--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter } from "react-router-dom";
 import NavLink from "react-router-dom/NavLink";
-import Drawer from "../../components//Drawer/Drawer";
+import Drawer from "../../components/Drawer/Drawer";
 
 export default class Navbar extends Component<any, any> {
   state = {
